d236b0374a5e0091d7e345f52f4056abbd182c9b
mitulgala4 <39183099+mitulgala4@users.noreply.github.com>
2018-05-13 14:57:03 +0530

    Add files via upload

---
+++ contact_form_handler.php
@@ -0,0 +1,22 @@
+<?php
+	$name = $_Post['name'];
+	$visitor_email = $_Post['email'];
+	$telephone = $_Post['telephone'];
+	$message = $_Post['message'];
+	
+	$email_from = 'amitgala4@gmail.com';
+	$email_subject = "New Form Submission";
+	$email_body = "User Name: $name.\n".
+					"User Email: $visitor_emailemail.\n".
+					"User Number: $telephone.\n".
+					"User Message: $message.\n";
+					
+	$to = "jproadsideassistance@gmail.com";
+
+	$headers = "From: $email_from \r\n";
+	$headers = "Reply-To:$visitor_email \r\n;
+	mail($to,$email_subject,$email_body,$headers);
+	header("Location: mital_webpage.html");
+	
+	
+	?>
